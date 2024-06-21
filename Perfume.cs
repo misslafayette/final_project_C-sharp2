0a2879f15f05d4ba569d5ae311ae1870c2791f5c
Anita <166741974+misslafayette@users.noreply.github.com>
2024-06-21 14:36:34 +0200

    added serialization

--- Perfume.cs
+++ Perfume.cs
@@ -2,18 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace final_project_C_sharp2
 {
     public class Perfume
     {
-        public string PerfumeName;
+        public string PerfumeName { get; set; }
+        public int Oil1Amount { get; set; }
+        public int Oil2Amount { get; set; }
+        public int Oil3Amount { get; set; }
+        public string Oil1 { get; set; }
+        public string Oil2 { get; set; }
+        public string Oil3 { get; set; }
+        public bool IncludeFields { get; set; }
 
-
-        public void FinalPerfume(string perfumeName)
+        public Perfume(string perfumeName, int oil1Amount, int oil2Amount, int oil3Amount, string oil1, string oil2, string oil3)
         {
             PerfumeName = perfumeName;
+            Oil1Amount = oil1Amount;
+            Oil2Amount = oil2Amount;
+            Oil3Amount = oil3Amount;
+            Oil1 = oil1;
+            Oil2 = oil2;
+            Oil3 = oil3;
         }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.Json;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
@@ -12,6 +13,8 @@ namespace final_project_C_sharp2
     {
         static void Main(string[] args)
         {
+            // user can't add their own essential oils
+            // these are all set by me and any of them may be used in the perfume
             Oil jasmine = new Oil("jasmine", "top", "floral");
             Oil vanilla = new Oil("vanilla", "base", "sweet");
             Oil magnolia = new Oil("magnolia", "middle", "floral");
@@ -23,16 +26,18 @@ namespace final_project_C_sharp2
             Oil lemon = new Oil("lemon", "top", "citrusy");
             Oil orange = new Oil("orange", "middle", "citrusy");
 
+            // creating a list that includes all the essential oils
             List<Oil> oilDatabase = new List<Oil> { jasmine, vanilla, magnolia, sandalwood,
                 cedarwood, lily, angelWings, cinnamon, lemon, orange };
 
+            // sorting the oils alphabetically
             var oilsAlphabetically = oilDatabase.OrderBy(r => r.OilName);
             foreach (Oil oil in oilsAlphabetically)
             {
                 Console.WriteLine(oil.OilName);
             }
-
             
+            // grouping the oils by composition of the perfume (i.e. if they form the base, middle, or top of the perfume)
            var oilsByComposition = oilDatabase.GroupBy(r => r.CompositionPart);
             foreach (var groupOfOils in oilsByComposition)
             {
@@ -43,6 +48,7 @@ namespace final_project_C_sharp2
                 }
             }
 
+            // grouping the oils by type of smells (i.e. floral, woodsy, citrusy, etc.)
             var oilsByScentType = oilDatabase.GroupBy(r => r.ScentType);
             foreach (var groupOfOils in oilsByScentType)
             {
@@ -52,72 +58,89 @@ namespace final_project_C_sharp2
                     Console.WriteLine(oil.OilName);
                 }
             }
-           
 
-            Perfume seraphine = new Perfume();
-            seraphine.PerfumeName = "Séraphine";
-
+            // creating or loading a perfume
             Console.WriteLine("To create a new perfume, type 'N' and press enter. To load a saved perfume, type 'L' and press enter.");
-            if (Console.ReadLine() == "N")
+            while (true)
             {
-                Console.WriteLine("How many oils will your perfume use?");
-                int num1 = 3;    // setting this variable to 3 on purpose, each perfume needs a base, middle, and top note, so this is the minimum
-                bool isInteger1 = int.TryParse(Console.ReadLine(), out num1);
-                int i = 0;
-                for (i = 1; i <= num1; i++)
+                if (Console.ReadLine() == "N")
                 {
-                    Console.WriteLine("Perfume:");
-                    string oilIngredient = Console.ReadLine();
-                    Console.WriteLine($"Drop count of {oilIngredient} essential oil:");
-                    int num2 = 0;
-                    bool isInteger2 = int.TryParse(Console.ReadLine(), out num2);
-                    while (!isInteger2)
+                    Console.WriteLine("How many oils will your perfume use?");
+                    List<int> dropCounts = new List<int>();        // creating a list to store drop counts from the for loop below
+                    List<string> oilNames = new List<string>();    // creating a list to store list of essential oils the user chooses
+                    int num1 = 3;   // set to 3 because the possible number of essential oils is 3 right now
+                    bool isInteger1 = int.TryParse(Console.ReadLine(), out num1);
+                    int i = 0;
+                    for (i = 1; i <= num1; i++)
                     {
-                        Console.WriteLine("Please enter a whole number:");
-                        isInteger2 = int.TryParse(Console.ReadLine(), out num2);
+                        Console.WriteLine("Essential oil name:");
+                        string oilIngredient = Console.ReadLine();
+                        oilNames.Add(oilIngredient);  // add the ingredient to the list
+                        Console.WriteLine($"Drop count of {oilIngredient} essential oil:");
+                        int num2 = 0;
+                        bool isInteger2 = int.TryParse(Console.ReadLine(), out num2);
+                        while (!isInteger2)
+                        {
+                            Console.WriteLine("Please enter a whole number:");
+                            isInteger2 = int.TryParse(Console.ReadLine(), out num2);
+                        }
+                        dropCounts.Add(num2);  // add the ingredient count to the list
                     }
 
+                    // i included this part to test out that the variables from the for loop above were saved correctly
+                    foreach (string oilName in oilNames)
+                    {
+                        Console.WriteLine(oilName);
+                    }
+                    foreach (var drops in dropCounts)
+                    {
+                        Console.WriteLine(drops);
+                    }
+
+                    // naming the perfume and writing out the full recipe
+                    Console.WriteLine("What will you name your new perfume?");
+                    string perfumeName = Console.ReadLine();
+                    Console.WriteLine($"Congratulations! Here is your complete perfume recipe for {perfumeName}.");
+                    Console.WriteLine($"{dropCounts[0]} {oilNames[0]}"); // pulling the information our by index to match the right oil with the right number of drops needed.
+                    Console.WriteLine($"{dropCounts[1]} {oilNames[1]}");
+                    Console.WriteLine($"{dropCounts[2]} {oilNames[2]}");
+                    Console.WriteLine($"Top with 10mL of carrier oil.");
+
+                    Perfume perfume = new Perfume($"{perfumeName}", dropCounts[0], dropCounts[1], dropCounts[2],
+                        oilNames[0], oilNames[1], oilNames[2]);
+
+                    // saving a file
+                    string filePath = $@"C:\Users\aaand\source\repos\Czechitas_CSharp2_2024\{perfumeName}.json";
+                    string jsonString = JsonSerializer.Serialize(perfume);
+                    File.WriteAllText(filePath, jsonString);
+                    Console.WriteLine("Your perfume recipe has been saved.");
+                    Console.WriteLine("To create a new perfume, type 'N' and press enter. To load a saved perfume, type 'L' and press enter.");
+
+                    // stream
+                    /*
+                    string filePath2 = Path.Combine(folder, "file2.txt");
+                    using (StreamWriter writer = new StreamWriter(filePath2, append: true))
+                    {
+                        writer.WriteLine("stuff to save");
+                    }
+                    */
+
                 }
 
-                Console.WriteLine("What will you name your new perfume?");
-                string perfumeName = Console.ReadLine();
-                Console.WriteLine($"Congratulations! Here is your complete perfume recipe for {perfumeName}.");
-                
-                // saving a file, serialization
-                string folder = @"C:\Users\aaand\source\repos\Czechitas_CSharp2_2024";
-                string filePath = Path.Combine(folder, "file1.txt");
-                File.WriteAllText(filePath, "");
-
-                // stream
-                string filePath2 = Path.Combine(folder, "file2.txt");
-                using (StreamWriter writer = new StreamWriter(filePath2, append: true))
+                // loading a file
+                else if (Console.ReadLine() == "L")
                 {
-                    writer.WriteLine("stuff to save");
+                    string fileName = $"{Console.ReadLine()}.json";
+                    string jsonString = File.ReadAllText(fileName);
+                    Perfume serapihne = JsonSerializer.Deserialize<Perfume>(jsonString);
+                    Console.WriteLine("To create a new perfume, type 'N' and press enter. To load a saved perfume, type 'L' and press enter.");
                 }
-                Console.WriteLine("Your perfume recipe has been saved.");
-
-                //serializace
-                /*
-                string filePath3 = Path.Combine(folder, "file3.txt");
-                JsonSerializer serializer = new JsonSerializer(typeof(List<Oil>));
-                using (StreamWriter writer2 = new StreamWriter(filePath3, append: true))
+                else
                 {
-                    serializer.Serialize(writer2, oilDatabase);
-                }*/
-
+                    Console.WriteLine("Invalid input.");
+                    Console.WriteLine("To create a new perfume, type 'N' and press enter. To load a saved perfume, type 'L' and press enter.");
+                }
             }
-
-            // deserializace
-            else if (Console.ReadLine() == 
-                "L")
-            {
-                
-            }
-            else
-            {
-                Console.WriteLine("Invalid input.");
-            }
-
         }
     }
 }
